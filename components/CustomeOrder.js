0173f816d9393f42bb1169d193813180b6aefff8
umaiss <mianumais1997@gmail.com>
2020-08-14 03:05:22 +0500

    custom commit

--- components/CustomeOrder.js
+++ components/CustomeOrder.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions} from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image, Dimensions, TextInput, Modal, TouchableHighlight } from 'react-native';
+import { responsiveWidth, responsiveHeight, responsiveFontSize } from 'react-native-responsive-dimensions';
+import DropDownPicker from 'react-native-dropdown-picker';
 import ImagePicker from 'react-native-image-picker';
-import {Appbar } from 'react-native-paper';
+import { Appbar } from 'react-native-paper';
+import { Form, Textarea } from 'native-base';
 var { width } = Dimensions.get("window")
 export default class CustomeOrder extends Component {
   constructor(props) {
     super(props);
     this.state = {
       filePath: {},
+      modalVisible: false,
+      country: 'uk'
     };
   }
-
+  toggleModal(visible) {
+    this.setState({ modalVisible: visible });
+  }
   chooseFile = () => {
     var options = {
       title: 'Select Image',
@@ -46,57 +53,140 @@ export default class CustomeOrder extends Component {
   render() {
     return (
       <View style={styles.Container}>
-        
-        <Appbar.Header style={{backgroundColor:'#BE1E2D'}}>
-           
-           <Appbar.BackAction
-             onPress={() =>this.props.navigation.goBack()}
-           />
-           <Appbar.Content
-             title="Custom Order"
-           />
-         </Appbar.Header>
-         <View style={styles.container}>
-   
-      <TouchableOpacity style={styles.buttonContainer}
-               onPress={() => this.props.navigation.navigate('chat')}
-              >
-                <Text  style={{color:'white'}}>Talk to person</Text>  
-              </TouchableOpacity>  
+
+        <Appbar.Header style={{ backgroundColor: '#BE1E2D' }}>
+
+          <Appbar.BackAction
+            onPress={() => this.props.navigation.goBack()}
+          />
+          <Appbar.Content
+            title="Custom Order"
+          />
+        </Appbar.Header>
+        <View style={styles.container}>
+
+          <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => this.props.navigation.navigate('chat')}
+          >
+            <Text style={{ color: 'white' }}>Talk to person</Text>
+          </TouchableOpacity>
 
 
-              <TouchableOpacity style={styles.buttonContainer}
-              onPress={this.chooseFile}
-              >
-                <Text  style={{color:'white'}}>Upload picture</Text>  
-              </TouchableOpacity>  
+          <TouchableOpacity style={styles.buttonContainer}
+            onPress={this.chooseFile}
+          >
+            <Text style={{ color: 'white' }}>Upload picture</Text>
+          </TouchableOpacity>
 
           <Image
-            source={ this.state.filePath }
+            source={this.state.filePath}
             style={{ width: 250, height: 250 }}
           />
+          <View>
+          <Modal animationType={"slide"} transparent={false}
+            visible={this.state.modalVisible}
+            onRequestClose={() => { console.log("Modal has been closed.") }}>
+              <Text  style={{ textAlign:'center',fontWeight:'bold',fontSize:20 }}>Create custom order</Text>
+
+            <View>
 
 
-<TouchableOpacity style={{
-               backgroundColor:"#BE1E2D",
-               width:width-40,
-               alignItems:'center',
-               padding:10,
-               borderRadius:5,
-               margin:20
-             }}
-             onPress={()=>{this.props.navigation.navigate('PaymentsOptions')}}>
-             <Text style={{
-                 fontSize:24,
-                 fontWeight:"bold",
-                 color:'white'
-               }}>
-               CHECKOUT
-             </Text>
-           </TouchableOpacity>  
-         
+            <DropDownPicker
+    items={[
+        {label: '1 day', value: 'uk'},
+        {label: '2 day', value: 'france'},
+        {label:'3 day'},
+        {label:'4 day'},
+        {label:'5 day'},
+        {label:'6 day'},
+        {label:'7 day'},
+        {label:'8 day'},
+        {label:'9 day'},
+        {label:'10 day'},
+        {label:'11 day'},
+        {label:'12 day'},
+        {label:'13 day'},
+        {label:'14 day'},
+        {label:'15 day'},
+        {label:'16 day'},
+        {label:'17 day'},
+        {label:'18 day'},
+        {label:'19 day'},
+        {label:'20 day'},
+        {label:'21 day'},
+        {label:'22 day'},
+        {label:'23 day'},
+        {label:'24 day'},
+        {label:'25 day'},
+        {label:'26 day'},
+        {label:'27 day'},
+        {label:'28 day'},
+        {label:'29 day'},
+        {label:'30 day'},
+        {label:'31 day'},
+    ]}
+    defaultValue={this.state.country}
+    containerStyle={{height: 40}}
+    style={{backgroundColor: '#fafafa'}}
+    itemStyle={{
+        justifyContent: 'flex-start'
+    }}
+    dropDownStyle={{backgroundColor: '#fafafa'}}
+    onChangeItem={item => this.setState({
+        country: item.value
+    })}
+/>
+              </View>
+              <TextInput
+                style={styles.inputField}
+                placeholder='Rs. price'
+                keyboardType='Default'
+                underlineColorAndroid="transparent"
+              />
+
+              <Form >
+                <Textarea rowSpan={5} bordered placeholder="Description" style={{ borderRadius: 10, marginTop: 10, backgroundColor: '#e6e6e6' }} />
+              </Form>
+
+              <TouchableHighlight onPress={() => {
+                this.toggleModal(!this.state.modalVisible)
+              }}>
+
+                <Text style={styles.text}>Next</Text>
+              </TouchableHighlight>
         
-</View>
+          </Modal>
+          </View>
+
+          <TouchableOpacity style={styles.buttonContainer}
+            onPress={() => { this.toggleModal(true) }}
+          >
+            <Text style={{ color: 'white' }}>create custom order</Text>
+          </TouchableOpacity>
+
+
+
+
+          <TouchableOpacity style={{
+            backgroundColor: "#BE1E2D",
+            width: width - 40,
+            alignItems: 'center',
+            padding: 10,
+            borderRadius: 5,
+            margin: 20
+          }}
+            onPress={() => { this.props.navigation.navigate('PaymentsOptions') }}>
+            <Text style={{
+              fontSize: 24,
+              fontWeight: "bold",
+              color: 'white'
+            }}>
+              CHECKOUT
+             </Text>
+          </TouchableOpacity>
+
+
+        </View>
 
 
 
@@ -108,23 +198,36 @@ export default class CustomeOrder extends Component {
 }
 
 const styles = StyleSheet.create({
-Container:{
-    flex:1,
-},
-container:{
-  marginTop:'30%',
-  justifyContent:'center',
-  alignItems:'center'
-},
+  Container: {
+    flex: 1,
+  },
+  container: {
+    marginTop: '30%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
 
-    buttonContainer: {
-        marginTop:6,
-        height:35,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom:7,
-        width:300,
-        backgroundColor: "#BE1E2D",
-      },
+  buttonContainer: {
+    marginTop: 6,
+    height: 35,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 7,
+    width: 300,
+    backgroundColor: "#BE1E2D",
+  },
+
+  text: {
+    color: '#3f2949',
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+  inputField: {
+    height: responsiveHeight(6),
+    width: responsiveWidth(100),
+    fontSize: 15,
+    marginVertical:'5%',
+    backgroundColor:'#e6e6e6'
+  }
 })
